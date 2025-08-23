742c818a14d506fe34600bc633a1238b85eb7af9
Jeremy Soller <jackpot51@gmail.com>
2025-08-22 20:21:56 -0600

    Revert "amd: drop support for LLVM 15, 16, 17"
    This reverts commit 82047fa82f1bbbbd4f1a9b05c034f79a00d85e31.

--- src/amd/vulkan/nir/radv_nir_rt_common.c
+++ src/amd/vulkan/nir/radv_nir_rt_common.c
@@ -9,6 +9,10 @@
 #include "radv_debug.h"
 #include "nir_builder.h"
 
+#if AMD_LLVM_AVAILABLE
+#include <llvm/Config/llvm-config.h>
+#endif
+
 static nir_def *build_node_to_addr(struct radv_device *device, nir_builder *b, nir_def *node, bool skip_type_and);
 
 static void
