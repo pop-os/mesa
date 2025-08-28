06408f210d923c304f512a3ddee8973c488b4963
Simon McVittie <smcv@debian.org>
2025-08-28 20:02:42 +0100

    d/merge-vulkan-driver-manifests.sh: Remove library_arch from JSON manifests
    Since 25.2.1 they differ slightly between architectures, having added
    a library_arch field. That's the only difference, so for now just
    remove it, while a longer-term solution is discussed on mesa/mesa#13745.
    
    Closes: #1110523

--- debian/merge-vulkan-driver-manifests.sh
+++ debian/merge-vulkan-driver-manifests.sh
@@ -7,7 +7,8 @@
 #
 # If the JSON manifest describing a Vulkan driver contains for example
 # "library_path": "/usr/lib/x86_64-linux-gnu/libvulkan_lvp.so"
-# then replace it with
+# "library_arch": "64"
+# then replace it with just
 # "library_path": "libvulkan_lvp.so"
 # to get the same content for each architecture, and rename from for example
 # "lvp_icd.x86_64.json"
@@ -28,6 +29,7 @@ for file in "$DESTDIR"/usr/share/vulkan/icd.d/*.*.json; do
 	if grep -q 'library_path.*/usr/lib/'"${DEB_HOST_MULTIARCH}"'/lib[^/"]*\.so' "${file}"; then
 		replacement="${file%.*.json}.json"
 		sed -E -e '/library_path/ s,/usr/lib/'"${DEB_HOST_MULTIARCH}"'/(lib[^/"]*\.so),\1,' \
+			-e '/library_arch/d' \
 			< "${file}" > "${replacement}"
 		diff -s -u "${file}" "${replacement}" || true
 		rm "${file}"
